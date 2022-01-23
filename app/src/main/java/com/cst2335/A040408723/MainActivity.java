8c922beaa5b9302d91e5590c21e76f021acf26df
Jin Zhang <zhan0725@algonquinlive.com>
2022-01-23 17:29:58 -0500

    lab2

--- app/src/main/java/com/cst2335/A040408723/MainActivity.java
+++ app/src/main/java/com/cst2335/A040408723/MainActivity.java
@@ -3,12 +3,34 @@ package com.cst2335.A040408723;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    //add onClickListener
+        //Button button = findViewById(R.id.button1);
+        //if (button != null) {
+            //button.setOnClickListener(new View.OnClickListener() {
+               // @Override
+                //public void onClick(View view) {
+                    //Toast.makeText(MainActivity.this, "Here is more information", Toast.LENGTH_LONG).show();
+                //}
+
+                //String toastMessage = MainActivity.this.getResources().getString(R.string.toast_message);
+           // });
+
+
+        //}
+
     }
 }

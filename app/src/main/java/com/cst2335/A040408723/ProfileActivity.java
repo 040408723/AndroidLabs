3b0829698ea78c736f0cb30251aeddc14d60455d
Jin Zhang <zhan0725@algonquinlive.com>
2022-02-09 14:57:05 -0500

    lab3 with updated ProfileActivity.java

--- app/src/main/java/com/cst2335/A040408723/ProfileActivity.java
+++ app/src/main/java/com/cst2335/A040408723/ProfileActivity.java
@@ -4,18 +4,13 @@ package com.cst2335.A040408723;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-
-
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,21 +20,9 @@ import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    ImageView imgView=findViewById(R.id.imagebutton1);
+
     public static final String TAG="PROFILE_ACTIVITY";
-
-    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    Intent data = result.getData();
-                    assert data != null;
-                    Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
-
-                    imgView.setImageBitmap(imgbitmap);
-                } else if (result.getResultCode() == Activity.RESULT_CANCELED)
-                    Log.i(TAG, "User refused to capture a picture.");
-            });
+    ImageView imgView;
 
     @SuppressLint("QueryPermissionsNeeded")
     @Override
@@ -50,18 +33,12 @@ public class ProfileActivity extends AppCompatActivity {
         Intent fromMain=getIntent();
         String msg=fromMain.getStringExtra("EMAIL");
 
-
-        TextView emailEditText=(TextView)findViewById(R.id.email2);
-        emailEditText.setText(msg);
-
         ImageButton btn = findViewById(R.id.imagebutton1);
-
 
         btn.setOnClickListener(view -> {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            ActivityResultLauncher<Intent> myPictureTakerLauncher = null;
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                assert false;
+
                 myPictureTakerLauncher.launch(takePictureIntent);
             }
         });
@@ -69,7 +46,18 @@ public class ProfileActivity extends AppCompatActivity {
         Log.e(TAG, "onCreate");
     }
 
+    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
 
+                    Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+
+                    imgView.setImageBitmap(imgbitmap);
+                } else if (result.getResultCode() == Activity.RESULT_CANCELED)
+                    Log.i(TAG, "User refused to capture a picture.");
+            });
 
             protected void onStart () {
                 super.onStart();

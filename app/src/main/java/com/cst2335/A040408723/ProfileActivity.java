c5fcf6580be5fc75cd00ee0fbe2aa072f47dd600
Jin Zhang <zhan0725@algonquinlive.com>
2022-02-09 23:52:08 -0500

    updated ProfileActivity.java for lab3

--- app/src/main/java/com/cst2335/A040408723/ProfileActivity.java
+++ app/src/main/java/com/cst2335/A040408723/ProfileActivity.java
@@ -1,19 +1,20 @@
 package com.cst2335.A040408723;
 
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -21,39 +22,34 @@ public class ProfileActivity extends AppCompatActivity {
 
     public static final String TAG="PROFILE_ACTIVITY";
     ImageView imgView;
-
-    @SuppressLint("QueryPermissionsNeeded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        ImageButton btn=findViewById(R.id.imagebutton1);
 
-        ImageButton btn = findViewById(R.id.imagebutton1);
+            btn.setOnClickListener(view ->{
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    myPictureTakerLauncher.launch(takePictureIntent);
+                }
+            });
 
-        btn.setOnClickListener(view -> {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-
-                myPictureTakerLauncher.launch(takePictureIntent);
-            }
-        });
 
         Log.e(TAG, "onCreate");
+
+        Intent fromMain=getIntent();
+        String emailInfo=fromMain.getStringExtra("EMAIL");
+        EditText emailEditText=findViewById(R.id.email22);
+        emailEditText.setText(emailInfo);
+
     }
+
 
     ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    Intent data = result.getData();
-
-                    Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
-
-                    imgView.setImageBitmap(imgbitmap);
-                } else if (result.getResultCode() == Activity.RESULT_CANCELED)
-                    Log.i(TAG, "User refused to capture a picture.");
-            });
+            this::onActivityResult);
 
             protected void onStart () {
                 super.onStart();
@@ -80,5 +76,14 @@ public class ProfileActivity extends AppCompatActivity {
                 Log.e(TAG, "onDestroy");
             }
 
+    private void onActivityResult(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+            imgView=(ImageView) findViewById(R.id.imagebutton1);
+            imgView.setImageBitmap(imgbitmap);
+        } else if (result.getResultCode() == Activity.RESULT_CANCELED)
+            Log.i(TAG, "User refused to capture a picture.");
+    }
 }
 

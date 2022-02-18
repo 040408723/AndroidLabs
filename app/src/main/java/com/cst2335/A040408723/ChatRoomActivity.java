94522df96e723a276beb8048ff9b7dbc67880d78
Jin Zhang <zhan0725@algonquinlive.com>
2022-02-18 14:55:03 -0500

    lab4 View.xml

--- app/src/main/java/com/cst2335/A040408723/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/A040408723/ChatRoomActivity.java
@@ -1,127 +1,143 @@
 package com.cst2335.A040408723;
 
-import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class ChatRoomActivity extends AppCompatActivity {
 
     Button sendButton;
     Button receiveButton;
     EditText typeMessage;
-    RecyclerView rView;
-    MyAdapter theAdapter;
 
-    ArrayList<Message> messages=new ArrayList<>();
 
+    private ArrayList<Message> list = new ArrayList<>();
+    MyListAdapter myAdapter;
+
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        sendButton=findViewById(R.id.buttonSend);
-        receiveButton=findViewById(R.id.buttonReceive);
-        typeMessage=findViewById(R.id.edittext1);
-        rView=findViewById(R.id.myRecyclerView);
-        theAdapter=new MyAdapter();
-        rView.setAdapter(theAdapter);
+        sendButton = findViewById(R.id.buttonSend);
+        receiveButton = findViewById(R.id.buttonReceive);
+        typeMessage = findViewById(R.id.edittext1);
 
-        rView.setLayoutManager(new LinearLayoutManager(this));
+        ListView myListView = findViewById(R.id.listview1);
+        myListView.setAdapter(myAdapter = new MyListAdapter());
 
+        sendButton.setOnClickListener(click -> {
 
-        sendButton.setOnClickListener(click->{
-            String typeMessage=edit.getText().toString();
-            if(!typeMessage.isEmpty()){
-                messages.add(new Message(typeMessage));
-                edit.setText("");
-                theAdapter.notifyItemInserted(messages.size()-1);
+            String typeText = typeMessage.getText().toString();
+            View image = findViewById(R.id.send);
+
+            if (!typeText.isEmpty()) {
+                Message newMsg = new Message(true, typeText);
+                list.add(newMsg);
+                myAdapter.notifyDataSetChanged();
+
             }
         });
 
-        receiveButton.setOnClickListener(click->{
-            String typeMessage=edit.getText().toString();
-            if(!typeMessage.isEmpty()){
-                messages.add(new Message(typeMessage));
-                edit.setText("");
-                theAdapter.notifyItemInserted(messages.size()-1);
+        receiveButton.setOnClickListener(click -> {
+            String typeText = typeMessage.getText().toString();
+            View image = findViewById(R.id.receive);
+
+            if (!typeText.isEmpty()) {
+
+                Message newMsg = new Message(false, typeText);
+                list.add(newMsg);
+                myAdapter.notifyDataSetChanged();
             }
         });
     }
 
+    private class MyListAdapter extends BaseAdapter {
+        List<Message> messages=new ArrayList<Message>();
 
-    public class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
-
-        @NonNull
-        @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-            LayoutInflater li=getLayoutInflater();
-            View thisRow=li.inflate(R.layout.message, parent, false);
-            return new MyViewHolder(thisRow);
+        public void add(Message message){
+            this.messages.add(message);
+            notifyDataSetChanged();
         }
-
         @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            Message thisRow=messages.get(position);
-            holder.messageView.setText(thisRow.getMessageTyped());
+        public int getCount() {
+            return list.size();
         }
 
         @Override
-        public int getItemCount() {
-            return messages.size();
-        }
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageButton sendButton=findViewById(R.id.send);
-        ImageButton receiveButton=findViewById(R.id.receive);
-        TextView sendMessage=findViewById(R.id.sendmessage);
-        TextView receiveMessage=findViewById(R.id.receivemessage);
-        public MyViewHolder(View itemView){
-            super(itemView);
-            itemView.setOnClickListener(click->{
-                int position=getAdapterPosition();
-
-            });
-
-
-
-    }
-
-
-    public class Message {
-        String sendMessage;
-        String receiveMessage;
-
-        public Message(String sendMessage, String receiveMessage) {
-            this.sendMessage = sendMessage;
-            this.receiveMessage = receiveMessage;
+        public Object getItem(int i) {
+            return messages.get(i);
         }
 
-        public String getSendMessage() {
-            return sendMessage;
+        @Override
+        public long getItemId(int position) {
+            return position;
         }
 
-        public String getReceiveMessage() {
-            return receiveMessage;
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            MessageViewHolder holder = new MessageViewHolder();
+            View newView = inflater.inflate(R.layout.message, parent, false);
+            LayoutInflater inflater = getLayoutInflater();
+            Message message=messages.get(position);
+            if(message.isSendOrReceive()) {
+                holder.avatar = findViewById(R.id.send);
+
+                newView.setTag(holder);
+                holder.messageBody.setText(message.getText());
+            }else{
+                holder.avatar=findViewById(R.id.receive);
+                newView.setTag(holder);
+                holder.messageBody.setText(message.getText());
+            }
+            return newView;
+
+
         }
-    }
+
+        class MessageViewHolder {
+            public View avatar;
+            public TextView messageBody;
+
+        }
+
+        public class Message {
+            public boolean sendOrReceive;
+            String msgType;
+
+            public Message(boolean sendOrReceive, String msgType) {
+                this.msgType = msgType;
+                this.sendOrReceive = sendOrReceive;
+
+            }
+
+            public String getMsgType() {
+                return msgType;
+            }
+
+            public boolean isSendOrReceive() {
+                return sendOrReceive;
+            }
+
+        }
     }
 }
+
+

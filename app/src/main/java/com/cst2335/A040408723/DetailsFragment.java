980e12641577c15dbbcfa168556d6ea804864d78
Jin Zhang <zhan0725@algonquinlive.com>
2022-03-13 18:33:14 -0400

    DetailsFragment.java

---
+++ app/src/main/java/com/cst2335/A040408723/DetailsFragment.java
@@ -0,0 +1,53 @@
+package com.cst2335.A040408723;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class DetailsFragment extends Fragment {
+
+    int position = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState == null){
+            // Get back arguments
+            if(getArguments() != null) {
+                position = getArguments().getInt("position", 0);
+            }
+        }
+    }
+
+    private static TextView textview1;
+    private static TextView textview2;
+    private static TextView checkbox1;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_details,container,false);
+        textview1=view.findViewById(R.id.here);
+        textview2=view.findViewById(R.id.id);
+        checkbox1=view.findViewById(R.id.checkboxmsg);
+
+        return view;
+    }
+    //Button button=findViewById(R.id.hide);
+    //button.setOnClickListener(click->{
+        //FragmentManager fragmentManager=getSupportFragmentManager();
+        //fragmentManager.beginTransaction()
+               // .setReorderingAllowed(true)
+                //.replace(R.id.flContainer1, secondFragment)
+               // .addToBackStack(null)
+                //.commit();
+    //})
+}

4dddd20e269e31fc5bb0e755493d6eaee87b75a3
Jin Zhang <zhan0725@algonquinlive.com>
2022-03-25 22:22:14 -0400

    lab6 final

--- app/src/main/java/com/cst2335/A040408723/TestToolbar.java
+++ app/src/main/java/com/cst2335/A040408723/TestToolbar.java
@@ -3,7 +3,12 @@ package com.cst2335.A040408723;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class TestToolbar extends AppCompatActivity {
 
@@ -15,4 +20,42 @@ public class TestToolbar extends AppCompatActivity {
         Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(tBar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+       // inflater.inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = null;
+        //Look at your menu XML file. Put a case for every id in that file:
+        switch(item.getItemId())
+        {
+            //what to do when the menu item is selected:
+
+            case R.id.airport_item:
+                message = "You clicked on item 1";
+                break;
+            case R.id.gasstation_item:
+                message = "You clicked on item 2";
+                break;
+            case R.id.twitter_item:
+                message = "You clicked on item 3";
+                break;
+            case R.id.call_item:
+                message = "You clicked on call";
+                Intent i = new Intent (getApplicationContext(), ChatRoomActivity.class);
+                startActivity(i);
+                break;
+        }
+        if ( message != null ) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
 }

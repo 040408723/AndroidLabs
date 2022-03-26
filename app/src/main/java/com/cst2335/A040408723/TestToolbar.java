2a0c8299fe77f669132c7c345d0719700d8b894b
Jin Zhang <zhan0725@algonquinlive.com>
2022-03-26 18:45:54 -0400

    lab7 nav bar

--- app/src/main/java/com/cst2335/A040408723/TestToolbar.java
+++ app/src/main/java/com/cst2335/A040408723/TestToolbar.java
@@ -1,7 +1,11 @@
 package com.cst2335.A040408723;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +14,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class TestToolbar extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class TestToolbar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class TestToolbar extends AppCompatActivity {
 
         Toolbar tBar = findViewById(R.id.toolbar);
         setSupportActionBar(tBar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -38,24 +53,45 @@ public class TestToolbar extends AppCompatActivity {
             //what to do when the menu item is selected:
 
             case R.id.airport_item:
-                message = "You clicked on item 1";
-                break;
-            case R.id.jumper_item:
-                message = "You clicked on item 2";
+                message = "You clicked on item 1 airport";
                 break;
             case R.id.bridge_item:
-                message = "You clicked on item 3";
+                message = "You clicked on item 2 bridge";
                 break;
-            /*case R.id.call_item:
-                message = "You clicked on call";
-                Intent i = new Intent (getApplicationContext(), ChatRoomActivity.class);
-                startActivity(i);
-                break;*/
+            case R.id.jumper_item:
+                message = "You clicked on item 3 jumper";
+                break;
+            case R.id.embassy_item:
+                message = "You clicked on the overflow menu embassy";
+                break;
+
         }
+
         if ( message != null ) {
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         }
         return true;
     }
 
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.chatroom_item:
+                Intent c = new Intent (getApplicationContext(), ChatRoomActivity.class);
+                startActivity(c);
+                break;
+            case R.id.weather_item:
+                Intent w = new Intent ();
+                break;
+            case R.id.goback_item:
+                Intent g = new Intent();
+                break;
+        }
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return false;
+    }
 }
